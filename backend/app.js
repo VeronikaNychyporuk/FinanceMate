bd358801a78534bd2ca0496b3d1d9ba20444e183
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2025-03-26 13:11:16 +0200

    server startuo settings

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-const userRoutes = require('./routes/userRoutes'); // приклад
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/users', userRoutes); // приклад
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
   .then(() => {
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.error(err));
